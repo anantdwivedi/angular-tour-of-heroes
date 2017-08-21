3f858f39f4ecafda5f2a4808ba88846f6ad2fb94
anant <anantdwivedi11@gmail.com>
2017-08-21 09:16:05 -0700

    angular Hero

---
+++ src/app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Mr. Nice', power: "", alterEgo: "" },
+    { id: 12, name: 'Narco', power: "", alterEgo: "" },
+    { id: 12, name: 'Bombasto', power: "", alterEgo: "" },
+    { id: 14, name: 'Celeritas', power: "", alterEgo: "" },
+    { id: 15, name: 'Magneta', power: "", alterEgo: "" },
+    { id: 16, name: 'RubberMan', power: "", alterEgo: "" },
+    { id: 17, name: 'Dynama', power: "", alterEgo: "" },
+    { id: 18, name: 'Dr IQ', power: "", alterEgo: "" },
+    { id: 19, name: 'Magma', power: "", alterEgo: "" },
+    { id: 20, name: 'Tornado', power: "", alterEgo: "" },
+];
+//# sourceMappingURL=mock-heroes.js.map
